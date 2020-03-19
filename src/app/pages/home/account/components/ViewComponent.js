f29cc479457d08508b1d2d15790ce2dc3cf6286d
jamewel <jbarit@softecdigital.com>
2020-03-20 06:04:39 +0800

    fix data tables on mobile responsive

--- src/app/pages/home/account/components/ViewComponent.js
+++ src/app/pages/home/account/components/ViewComponent.js
@@ -4,6 +4,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
@@ -11,10 +12,14 @@ import TableHead from '@material-ui/core/TableHead';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
-import { getAccountList } from '../../../../services/account.service';
+import Tooltip from '@material-ui/core/Tooltip';
+import Chip from '@material-ui/core/Chip';
+import { getBusiness, getBusinessById } from '../../../../services/business.service';
 
 const useStyles1 = makeStyles(theme => ({
   root: {
@@ -106,65 +111,100 @@ function Business(props) {
     setPage(0);
   };
 
+  const editBusiness = (id) => {
+    getBusinessById(id)
+      .then((results) => {
+        console.log(props)
+        props.props.history.push(`/business/${id}`, {
+          data: results.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <Paper>
-      <Table className={classes.table} aria-label="custom pagination table">
-      <TableHead>
-          <TableRow>
-            <TableCell>Account Name</TableCell>
-            <TableCell align="left">website</TableCell>
-            <TableCell align="center">Parent Account</TableCell>
-            <TableCell align="left">Country</TableCell>
-            <TableCell align="left">Active</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.name ? row.name : ''}
-              </TableCell>
-              <TableCell align="left">{row.website}</TableCell>
-              <TableCell align="left">{row.parentAccountId}</TableCell>
-              <TableCell align="left">{row.country}</TableCell>
-              <TableCell align="right"> {`${row.active}`} </TableCell>
+      <TableContainer>
+        <Table className={classes.table} aria-label="custom pagination table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="left">Business Name</TableCell>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="left">Phone</TableCell>
+              <TableCell align="right">Active</TableCell>
+              <TableCell align="right">Action</TableCell>
             </TableRow>
-          ))}
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map(row => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.name ? row.name : ''}
+                </TableCell>
+                <TableCell align="left">{row.businessName}</TableCell>
+                <TableCell align="left">{row.email}</TableCell>
+                <TableCell align="left">{row.phone}</TableCell>
+                <TableCell align="right">
+                  { row.active && 
+                    <Chip color="primary" label="Active" />
+                  }
+                  { !row.active && 
+                    <Chip color="secondary" label="Inactive" />
+                  }
+                </TableCell>
+                <TableCell align="right">
+                  <Tooltip title="Delete">
+                    <IconButton aria-label="Delete">
+                      <DeleteIcon />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Edit">
+                    <IconButton aria-label="Edit" onClick={ () => editBusiness(row.id) }>
+                      <EditIcon />
+                    </IconButton>
+                </Tooltip>
+                </TableCell>
+              </TableRow>
+            ))}
 
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                colSpan={3}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { 'aria-label': 'rows per page' },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
             </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: { 'aria-label': 'rows per page' },
-                native: true,
-              }}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 }
 
 
-class ViewComponent extends React.Component {
+class BusinessViewComponents extends React.Component {
 
   constructor(props){
     super(props);
@@ -174,18 +214,17 @@ class ViewComponent extends React.Component {
   }
 
   componentDidMount(){
-    getAccountList()
+    getBusiness()
       .then((results) => {
-        console.log(results);
         this.setState({row:results.data})
       })
   }
 
   render() {
     return(
-      <Business data={this.state}/>
+      <Business data={this.state} props={this.props}/>
     )
   }
 }
 
-export default ViewComponent;
+export default BusinessViewComponents;
--- src/app/services/business.service.js
+++ src/app/services/business.service.js
@@ -1,11 +1,28 @@
 import axios from "axios";
 
 export const ROOT_URL = "https://thecrmnetwork20200225035915.azurewebsites.net";
-export const GET_BUSINESS = "api/admin/business/table";
+export const ROOT_BUSINESS = "admin/business";
+export const GET_BUSINESS = "admin/business/table";
 
 export function getBusiness() {
   return axios.get(`${ROOT_URL}/${GET_BUSINESS}`);
 }
+
+export function getBusinessById(id){
+  return axios.get(`${ROOT_URL}/${ROOT_BUSINESS}/${id}`);
+}
+
+export function saveBusiness(business) {
+  return axios.post(`${ROOT_URL}/${ROOT_BUSINESS}`, business);
+}
+
+export function updateBusiness(business) {
+  return axios.patch(`${ROOT_URL}/${ROOT_BUSINESS}`,  business);
+}
+
+// export function editBusiness(id) {
+//   return axios.pat(`${ROOT_URL}/${SAVE_BUSINESS}`, { business });
+// }
 
 // export function getUserByToken() {
 //   // Authorization head should be fulfilled in interceptor.
