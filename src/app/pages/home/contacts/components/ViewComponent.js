2cbace3c70d38f78340771a92d66288827d2cf1b
Joel Ralph Balignasay <joelralph06@gmail.com>
2020-03-20 20:53:26 +0800

    changes on views

--- src/app/pages/home/contacts/components/ViewComponent.js
+++ src/app/pages/home/contacts/components/ViewComponent.js
@@ -1,191 +1,79 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableHead from '@material-ui/core/TableHead';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
-// import { getAccountList } from '../../../../services/account.service';
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+// import { getLeadsList, registerLead, updateLead, deleteLead} from '../../../../services/leads.service';
 
-const useStyles1 = makeStyles(theme => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
-
-  const handleFirstPageButtonClick = event => {
-    onChangePage(event, 0);
-  };
-
-  const handleBackButtonClick = event => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = event => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = event => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onChangePage: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
-
-let rows = [];
-
-const useStyles2 = makeStyles({
-  table: {
-    minWidth: 500,
-  },
-});
-
-function Contacts(props) {
-  rows = props.data.row;
-  const classes = useStyles2();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  return (
-    <Paper>
-      <Table className={classes.table} aria-label="custom pagination table">
-      <TableHead>
-          <TableRow>
-            <TableCell>Account Name</TableCell>
-            <TableCell align="left">website</TableCell>
-            <TableCell align="center">Parent Account</TableCell>
-            <TableCell align="left">Country</TableCell>
-            <TableCell align="left">Active</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
-          ).map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.name ? row.name : ''}
-              </TableCell>
-              <TableCell align="left">{row.website}</TableCell>
-              <TableCell align="left">{row.parentAccountId}</TableCell>
-              <TableCell align="left">{row.country}</TableCell>
-              <TableCell align="right"> {`${row.active}`} </TableCell>
-            </TableRow>
-          ))}
-
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: { 'aria-label': 'rows per page' },
-                native: true,
-              }}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </Paper>
-  );
-}
-
-
-class ViewComponent extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      row: []
+const ViewComponent = () => {
+  const [state, setState] = useState(0);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+    // const response = await getLeadsList();
+      setState({
+        columns: [
+          { title: 'Name', field: 'name' },
+          { title: 'Phone', field: 'phone' },
+          { title: 'Company Name', field: 'company'},
+          { title: 'Lead Source', field: 'leadSource'}
+        ],
+        // data : response.data
+      });
     }
-  }
+    fetchData();
+  }, []);
 
-//   componentDidMount(){
-//     getAccountList()
-//       .then((results) => {
-//         console.log(results);
-//         this.setState({row:results.data})
-//       })
-//   }
-
-  render() {
-    return(
-      <Contacts data={this.state}/>
-    )
-  }
+  return (
+    <MaterialTable
+      title="Accounts"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            // registerAccount(newData)
+            //   .then((result) => {
+            //     resolve();
+            //     setState(prevState => {
+            //       const data = [...prevState.data];
+            //       data.push(newData);
+            //       return { ...prevState, data };
+            //     });
+            //   })
+          }),
+        onRowUpdate: (newData, oldData) =>
+           new Promise((resolve, reject) => {
+            // updateAccount(newData)
+            //   .then((result) => {
+            //     resolve();
+            //     if (oldData) {
+            //       setState(prevState => {
+            //         const data = [...prevState.data];
+            //         data[data.indexOf(oldData)] = newData;
+            //         return { ...prevState, data };
+            //       });
+            //     }
+            //   })
+            //   .catch((err) => {
+            //     reject(err)
+            //   })
+          }),
+        onRowDelete: oldData =>
+          new Promise((resolve,reject) => {
+            // deleteAccount(oldData.id)
+            //   .then((results) => {
+            //     resolve();
+            //     setState(prevState => {
+            //       const data = [...prevState.data];
+            //       data.splice(data.indexOf(oldData), 1);
+            //       return { ...prevState, data };
+            //     });
+            //   })
+            //   .catch((err) => {
+            //     reject()
+            //   })
+          }),
+      }}
+    />
+  );
 }
 
 export default ViewComponent;
