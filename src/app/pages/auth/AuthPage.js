877596e23bd0b7d2d0cb4813ac640f15d80926c4
jamg <jammmg26@gmail.com>
2020-03-28 16:20:27 +0800

    fixed dashboard logo

--- src/app/pages/auth/AuthPage.js
+++ src/app/pages/auth/AuthPage.js
@@ -1,57 +1,58 @@
-import React from "react";
-import { Link, Switch, Route, Redirect } from "react-router-dom";
-import { toAbsoluteUrl } from "../../../_metronic";
-import "../../../_metronic/_assets/sass/pages/login/login-1.scss";
-import Login from "./Login";
-import Registration from "./Registration";
-import ForgotPassword from "./ForgotPassword";
-
+import React from 'react';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
+import { toAbsoluteUrl } from '../../../_metronic';
+import '../../../_metronic/_assets/sass/pages/login/login-1.scss';
+import Login from './Login';
+import Registration from './Registration';
+import ForgotPassword from './ForgotPassword';
 
 export default function AuthPage() {
   return (
     <>
-      <div className="kt-grid kt-grid--ver kt-grid--root">
+      <div className='kt-grid kt-grid--ver kt-grid--root'>
         <div
-          id="kt_login"
-          className="kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v1"
+          id='kt_login'
+          className='kt-grid kt-grid--hor kt-grid--root kt-login kt-login--v1'
         >
-          <div className="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop" 
-          >
+          <div className='kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop'>
             <div
-              className="kt-grid__item  kt-grid kt-grid--hor kt-login__aside"
-               style={{
-                
-              }}
+              className='kt-grid__item  kt-grid kt-grid--hor kt-login__aside'
+              style={{}}
             >
-              <div className="kt-grid__item">
-                <Link to="/" className="kt-login__logo">
+              <div className='kt-grid__item'>
+                <Link to='/' className='kt-login__logo'>
                   <img
-                    alt="Logo"
-                    src={toAbsoluteUrl("/media/logos/logo-thecrmnetwork.png")}
+                    alt='Logo'
+                    src={toAbsoluteUrl('/media/logos/logo-thecrmnetwork.png')}
                   />
                 </Link>
               </div>
-              <div className="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver" style={{justifyContent: 'center'}}>
-                <div className="kt-grid__item kt-grid__item--middle kt-title-container" >
-                  <h3 className="kt-login__title" >Welcome to The CRM Network</h3>
-{/*                  <h4 className="kt-login__subtitle" style={{color: '#67666e'}}>
+              <div
+                className='kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver'
+                style={{ justifyContent: 'center' }}
+              >
+                <div className='kt-grid__item kt-grid__item--middle kt-title-container'>
+                  <h3 className='kt-login__title'>
+                    Welcome to The CRM Network
+                  </h3>
+                  {/*                  <h4 className="kt-login__subtitle" style={{color: '#67666e'}}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                   </h4>*/}
                 </div>
               </div>
-              <div className="kt-grid__item">
-                <div className="kt-login__info">
-                  <div className="kt-login__copyright">
+              <div className='kt-grid__item'>
+                <div className='kt-login__info'>
+                  <div className='kt-login__copyright'>
                     &copy; 2020 The CRM Network
                   </div>
-                  <div className="kt-login__menu">
-                    <Link to="/terms" className="kt-link">
+                  <div className='kt-login__menu'>
+                    <Link to='/terms' className='kt-link'>
                       Privacy
                     </Link>
-                    <Link to="/terms" className="kt-link">
+                    <Link to='/terms' className='kt-link'>
                       Legal
                     </Link>
-                    <Link to="/terms" className="kt-link">
+                    <Link to='/terms' className='kt-link'>
                       Contact
                     </Link>
                   </div>
@@ -59,16 +60,16 @@ export default function AuthPage() {
               </div>
             </div>
 
-            <div className="kt-grid__item kt-grid__item--fluid  kt-grid__item--order-tablet-and-mobile-1  kt-login__wrapper">
+            <div className='kt-grid__item kt-grid__item--fluid  kt-grid__item--order-tablet-and-mobile-1  kt-login__wrapper'>
               <Switch>
-                <Route path="/auth/login" component={Login} />
-                <Route path="/auth/registration" component={Registration} />
+                <Route path='/auth/login' component={Login} />
+                <Route path='/auth/registration' component={Registration} />
                 <Route
-                  path="/auth/forgot-password"
+                  path='/auth/forgot-password'
                   component={ForgotPassword}
                 />
-                <Redirect from="/auth" exact={true} to="/auth/login" />
-                <Redirect to="/auth/login" />
+                <Redirect from='/auth' exact={true} to='/auth/login' />
+                <Redirect to='/auth/login' />
               </Switch>
             </div>
           </div>
