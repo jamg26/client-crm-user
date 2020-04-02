c5b43116ba68976d27f18f5d3714fd59b83538dd
Jamewel <chiejhay@gmail.com>
2020-04-02 23:18:44 +0800

    reset password

--- src/app/pages/auth/ResetPassword.js
+++ src/app/pages/auth/ResetPassword.js
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { FormattedMessage, injectIntl } from "react-intl";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Button, TextField, FormControl } from "@material-ui/core";
 import { changePassword } from '../../services/user.service';
 import { Link } from "react-router-dom";
+import { Alert, AlertTitle } from '@material-ui/lab';
 
-const ResetPassword = () => {
+const ResetPassword = (props) => {
+  let userId = props.match.params.id;
 
-  const notify = data => {
-      if (data.success) {
-          toast.success(data.message);
-      } else {
-          toast.error(data.message);
-      }
-  }
+  const [notifyAccept, setNotifyAccept] = useState('')
 
   const clearValidation = {
     error: false
   }
 
   const clearFormData = {
-    oldPassword: '',
+    userId: userId,
     newPassword: '',
     confirmPassword: ''
   }
@@ -37,44 +31,41 @@ const ResetPassword = () => {
     })
   }
 
+  const handleNotNow = e => {
+      window.location.href = "/"
+  }
+
+  const notifySuccess = <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+            Resetting password was successful.
+      </Alert>;
+
+  const notifyFailed = <Alert severity="error">
+        <AlertTitle>Failed</AlertTitle>
+            Resetting password was not successful..
+      </Alert>;
+
 
   const handleResetPassword = (e) => {
-    if (validateData()) {
-        // changePassword(formData)
-        //         .then( (data ) => {
-        //           if (data.data.result === 'Password did not match') {
-        //             setValidation({oldPassword : {
-        //         error: true,
-        //         errorMessage: 'Incorrect password.'
-        //       }
-        //     });
-        //           } else  {
-        //             notify({ success: true, message: 'Changing password successful.' });
-        //             setformData(clearFormData)
-        //           }
 
-        //         }).catch(() => {});
+    if (validateData()) {
+        changePassword(formData)
+                .then( (data ) => {
+                  if (data.data.status === 'Success') {
+                      setNotifyAccept(notifySuccess);
+                      setTimeout(() => { window.location.href = "/"; }, 2000);
+                  } else {
+                      setNotifyAccept(notifyFailed);
+                  }
+                  
+                }).catch(() => {});
     }
     
   } 
 
   const validateData = () => {
     setValidation(clearValidation);
-    if (formData.oldPassword === '') {
-      setValidation({oldPassword : {
-          error: true,
-          errorMessage: 'This field is required.'
-        }
-      });
-      return false;
-    } else if (formData.newPassword === '') {
-      setValidation({newPassword : {
-          error: true,
-          errorMessage: 'This field is required.'
-        }
-      });
-      return false
-    } else if (formData.newPassword.length < 8) {
+    if (formData.newPassword.length < 8) {
       setValidation({newPassword : {
           error: true,
           errorMessage: 'New password must container atleast 8 characters.'
@@ -109,91 +100,76 @@ const ResetPassword = () => {
           </h3>
         </div>
             <form className="kt-form kt-form--label-right" oValidate autoComplete="off">
-                    <div className="form-body">
-                      <div className="kt-portlet__body">
-                              <div className="form-group mb-4">
-                                  <TextField
-                                    margin="normal"
-                                    label="Old Password"
-                                    className="kt-width-full"
-                                    name="oldPassword"
-                                    type="password"
-                                    onChange={handleChange}
-                                    value={formData?.oldPassword}
-                                    helperText={validation?.oldPassword?.errorMessage}
-                                    error={validation?.oldPassword?.error}
-                                 />
-                            </div>
-                            <div className="form-group mb-4">
-                                  <TextField
-                                    margin="normal"
-                                    label="New Password"
-                                    className="kt-width-full"
-                                    name="newPassword"
-                                    type="password"
-                                    onChange={handleChange}
-                                    value={formData?.newPassword}
-                                    helperText={validation?.newPassword?.errorMessage}
-                                    error={validation?.newPassword?.error}
-                                 />
-                            </div>
-                            <div className="form-group mb-4">
-                                  <TextField
-                                    margin="normal"
-                                    label="Confirm Password"
-                                    className="kt-width-full"
-                                    name="confirmPassword"
-                                    type="password"
-                                    onChange={handleChange}
-                                    value={formData?.confirmPassword}
-                                    helperText={validation?.confirmPassword?.errorMessage}
-                                    error={validation?.confirmPassword?.error}
-                                 />
-                            </div>
-                            
+                <div className="form-body">
+                  <div className="kt-portlet__body">
+                      {notifyAccept}
+                        <div className="form-group mb-4">
+                              <TextField
+                                margin="normal"
+                                label="New Password"
+                                className="kt-width-full"
+                                name="newPassword"
+                                type="password"
+                                onChange={handleChange}
+                                value={formData?.newPassword}
+                                helperText={validation?.newPassword?.errorMessage}
+                                error={validation?.newPassword?.error}
+                             />
                         </div>
+                        <div className="form-group mb-4">
+                              <TextField
+                                margin="normal"
+                                label="Confirm Password"
+                                className="kt-width-full"
+                                name="confirmPassword"
+                                type="password"
+                                onChange={handleChange}
+                                value={formData?.confirmPassword}
+                                helperText={validation?.confirmPassword?.errorMessage}
+                                error={validation?.confirmPassword?.error}
+                             />
+                        </div>
+                        
                     </div>
+                </div>
             </form>
             
             <div className="kt-portlet__body">
-                    <div className="row">
-                      <div className="col-md-5"></div>
-                      <div className="col-md-2">
-                        <div className="form-group mb-4">
-                          <Link
-                                to="/"
-                              >
-                                <Button
-                                    type='submit'
-                                    size='large'
-                                    className='float-right'
-                                    variant='contained'
-                                    color='default'
-                                >
-                                    CANCEL
-                                </Button>
-                            </Link>
+                <div className="row">
+                  <div className="col-md-5"></div>
+                  <div className="col-md-2">
+                    <div className="form-group mb-4">
+      
+                          <Button
+                              type='submit'
+                              size='large'
+                              className='float-right'
+                              variant='contained'
+                              color='default'
+                              onClick={handleNotNow}
+                          >
+                              CANCEL
+                          </Button>
 
-                            </div>
-                        </div>
-                        <div className="col-md-5">
-                            <div className="form-group mb-4">
-                              <Button
-                                  type='submit'
-                                  size='large'
-                                  className='float-left'
-                                  variant='contained'
-                                  color='primary'
-                                  onClick={handleResetPassword}
-                              >
-                                  Reset Password
-                              </Button>
-                            </div>
+
                         </div>
                     </div>
-                  </div>
-            
-
+                    <div className="col-md-5">
+                        <div className="form-group mb-4">
+                          <Button
+                              type='submit'
+                              size='large'
+                              className='float-left'
+                              variant='contained'
+                              color='primary'
+                              onClick={handleResetPassword}
+                          >
+                              Reset Password
+                          </Button>
+                        </div>
+                    </div>
+                </div>
+              </div>
       </div>
     </div>
   )
