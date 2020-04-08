c3f16a0d44ee91b1dcad865d78053ef11818b08e
Jamuel Galicia <jammmg26@gmail.com>
2020-04-09 01:03:57 +0800

    adding of product

--- src/app/services/products.service.js
+++ src/app/services/products.service.js
@@ -1,9 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const ROOT_URL = process.env.REACT_APP_API_URL;
-export const PRODUCT_URL = "api/product/infobybusinessid";
+export const PRODUCT_URL = 'api/product/infobybusinessid';
+export const ADD_PRODUCT_URL = 'api/product/addnewproduct';
 
 export function getProductList(businessId) {
   return axios.get(`${ROOT_URL}/${PRODUCT_URL}/${businessId}`);
 }
 
+export function addProduct(data) {
+  return axios.post(`${ROOT_URL}/${ADD_PRODUCT_URL}`, data);
+}
--- src/app/store/actions/index.js
+++ src/app/store/actions/index.js
@@ -1,4 +1,3 @@
 export const addProducts = (data) => (dispatch) => {
-  console.log('wewewe');
   dispatch({ type: 'PRODUCT_ADDED', payload: data });
 };
